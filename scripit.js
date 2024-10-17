2b9c36044530fad34b1328653657e570aa4b0c3a
carlos <carlos.teixeira56@aluno.ce.gov.br>
2024-10-17 09:59:09 -0300

    Create scripit.js

---
+++ scripit.js
@@ -0,0 +1,8 @@
+let fila
+let opcao
+console.log('opcoes:')
+console.log('1 novo paciente')
+console.log("2. Consultar paciente")
+console.log("3. Sair")
+option = prompt ('escolha uma opcao (1,2 ou 3')
+ if (opcao)
